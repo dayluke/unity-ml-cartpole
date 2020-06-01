4cc61d4f08d2b66527dbada187e96812890b6b51
Luke Day <ld227707@falmouth.ac.uk>
2020-06-02 00:04:58 +0100

    Added a basic CartController script, which translates the cart left or right, when the user pressed 'a' or 'd', respectively.

---
+++ Assets/Scripts/CartController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartController : MonoBehaviour
+{
+    public new Rigidbody rigidbody;
+    public float speed;
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+    
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+        
+    }
+}
